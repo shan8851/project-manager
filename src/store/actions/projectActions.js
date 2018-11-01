5726778c29ada8d33cb5e0f52a57ec229c19b1ac
shan5742 <asamshan456@gmail.com>
2018-11-01 22:42:35 +0000

    creating project action

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,6 +1,6 @@
 export const createProject = project => {
   return (dispatch, getState) => {
     // make async call to database
-    dispatch({ type: "CREATE_Project", project });
+    dispatch({ type: "CREATE_PROJECT", project });
   };
 };
