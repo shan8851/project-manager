52429712893a2a86458557bc599a8625034088b3
shan5742 <asamshan456@gmail.com>
2018-11-01 16:05:41 +0000

    adding route paths and project details page

---
+++ src/components/projects/ProjectDetails.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const ProjectDetails = () => {
+  return (
+    <div className="container section project-details">
+      <div className="card z-depth-0">
+        <div className="card-content">
+          <span className="card-title">Project Title</span>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia
+            sapiente velit quisquam magni? Fugit eveniet iusto ipsum cum a
+            accusamus veritatis deleniti ipsa, atque facilis amet animi
+            molestiae dolores obcaecati?
+          </p>
+        </div>
+        <div className="card-action grey lighten-4 grey-text">
+          <div>Posted by Asam</div>
+          <div>2nd September 2am</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectDetails;
