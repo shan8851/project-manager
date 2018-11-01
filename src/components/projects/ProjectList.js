cc71a0417f8f90ac373ea9b1b5a0d73a73416e51
shan5742 <asamshan456@gmail.com>
2018-11-01 15:43:40 +0000

    adding dashboard, project list and notifications including styling and collumns

---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const ProjectList = () => {
+  return (
+    <div className="project-list section">
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by Asam Shan</p>
+          <p className="grey-text">3rd Septemper, 2am</p>
+        </div>
+      </div>
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by Asam Shan</p>
+          <p className="grey-text">3rd Septemper, 2am</p>
+        </div>
+      </div>
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by Asam Shan</p>
+          <p className="grey-text">3rd Septemper, 2am</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectList;
