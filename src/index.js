ca5d534b2a3f26f8703b45b8e3faff5cde3ac051
shan5742 <asamshan456@gmail.com>
2018-11-01 23:24:16 +0000

    adding firebase config files so that user can create new project which then updates firebase db

--- src/index.js
+++ src/index.js
@@ -3,14 +3,22 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./store/reducers/rootReducer";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
-import { getFirestore } from "redux-firestore";
-import { getFirebase } from "react-redux-firebase";
+import { reduxFirestore, getFirestore } from "redux-firestore";
+import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
+import fbConfig from "./config/fbConfig";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+    reactReduxFirebase(fbConfig),
+    reduxFirestore(fbConfig)
+  )
+);
 
 ReactDOM.render(
   <Provider store={store}>
--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,6 +1,21 @@
 export const createProject = project => {
-  return (dispatch, getState) => {
-    // make async call to database
-    dispatch({ type: "CREATE_PROJECT", project });
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
+    // make async
+    const firestore = getFirestore();
+    firestore
+      .collection("projects")
+      .add({
+        ...project,
+        authorFirstName: "Asam",
+        authorLastName: "Shan",
+        authorId: 12345,
+        createdAt: new Date()
+      })
+      .then(() => {
+        dispatch({ type: "CREATE_PROJECT", project });
+      })
+      .catch(err => {
+        dispatch({ type: "CREATE_PROJECT_ERROR", err });
+      });
   };
 };
