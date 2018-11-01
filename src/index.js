6693afb51879f4da7073ceffa3adf03e9525dafb
shan5742 <asamshan456@gmail.com>
2018-11-01 23:02:06 +0000

    adding firebase packages

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./store/reducers/rootReducer";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
+import { getFirestore } from "redux-firestore";
+import { getFirebase } from "react-redux-firebase";
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
