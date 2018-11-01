d924e6e32dbdc014f4240890d414721abaef11ba
shan5742 <asamshan456@gmail.com>
2018-11-01 16:08:17 +0000

    adding route paths and project details page

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-const ProjectDetails = () => {
+const ProjectDetails = props => {
+  const id = props.match.params.id;
   return (
     <div className="container section project-details">
       <div className="card z-depth-0">
         <div className="card-content">
-          <span className="card-title">Project Title</span>
+          <span className="card-title">Project Title - {id}</span>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia
             sapiente velit quisquam magni? Fugit eveniet iusto ipsum cum a
