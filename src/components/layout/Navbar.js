a7c38336b30bea752e209b5c33ab0eaf14008895
shan5742 <asamshan456@gmail.com>
2018-11-02 00:57:43 +0000

    showing different nav links for logged in and logged out users

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,26 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import SignedInLinks from "./SigneInLinks";
+import SignedInLinks from "./SignedInLinks";
 import SignedOutLinks from "./SignedOutLinks";
 import { connect } from "react-redux";
 
-const Navbar = () => {
+const Navbar = props => {
+  const { auth } = props;
+  const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />;
   return (
     <nav className="nav wrapper grey darken-3">
       <div className="container">
         <Link to="/" className="brand-logo">
           Project Management
         </Link>
-        <SignedInLinks />
-        <SignedOutLinks />
+        {links}
       </div>
     </nav>
   );
 };
 
 const mapStateToProps = state => {
-  console.log(state);
-  return {};
+  return {
+    auth: state.firebase.auth
+  };
 };
 
 export default connect(mapStateToProps)(Navbar);
