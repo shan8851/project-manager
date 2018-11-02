2140ed67e578a4ce61904992802ad04677a8101d
shan5742 <asamshan456@gmail.com>
2018-11-02 01:02:56 +0000

    delaying rendering of the dom until firebase has checked auth

--- src/index.js
+++ src/index.js
@@ -15,15 +15,17 @@ const store = createStore(
   rootReducer,
   compose(
     applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
-    reactReduxFirebase(fbConfig),
+    reactReduxFirebase(fbConfig, { attachAuthIsReady: true }),
     reduxFirestore(fbConfig)
   )
 );
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
-serviceWorker.unregister();
+store.firebaseAuthIsReady.then(() => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById("root")
+  );
+  serviceWorker.unregister();
+});
