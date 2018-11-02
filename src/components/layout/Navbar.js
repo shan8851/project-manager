900dc4be1a970cbb7e88abf3fcd420ec7dccacfd
shan5742 <asamshan456@gmail.com>
2018-11-02 00:12:34 +0000

    adding firebase auth and connecting it to state via console intitially

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import SignedInLinks from "./SigneInLinks";
 import SignedOutLinks from "./SignedOutLinks";
+import { connect } from "react-redux";
 
 const Navbar = () => {
   return (
     <nav className="nav wrapper grey darken-3">
       <div className="container">
         <Link to="/" className="brand-logo">
-          MarioPlan
+          Project Management
         </Link>
         <SignedInLinks />
         <SignedOutLinks />
@@ -17,4 +18,9 @@ const Navbar = () => {
   );
 };
 
-export default Navbar;
+const mapStateToProps = state => {
+  console.log(state);
+  return {};
+};
+
+export default connect(mapStateToProps)(Navbar);
