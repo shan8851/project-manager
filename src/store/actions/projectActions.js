007755977a9ef32fcad5ea9de7fae713dca885ff
shan5742 <asamshan456@gmail.com>
2018-11-02 02:06:37 +0000

    getting correct author details

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -2,13 +2,15 @@ export const createProject = project => {
   return (dispatch, getState, { getFirebase, getFirestore }) => {
     // make async
     const firestore = getFirestore();
+    const profile = getState().firebase.profile;
+    const authorId = getState().firebase.auth.uid;
     firestore
       .collection("projects")
       .add({
         ...project,
-        authorFirstName: "Asam",
-        authorLastName: "Shan",
-        authorId: 12345,
+        authorFirstName: profile.firstName,
+        authorLastName: profile.lastName,
+        authorId: authorId,
         createdAt: new Date()
       })
       .then(() => {
