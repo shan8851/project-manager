106760231afacd2f69f9a0e7d8fd4610b9f001a0
shan5742 <asamshan456@gmail.com>
2018-11-02 12:34:37 +0000

    adding firebase cloud functions

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import Notifications from "./Notifications";
 import ProjectList from "../projects/ProjectList";
+import Notifications from "./Notifications";
 import { connect } from "react-redux";
 import { firestoreConnect } from "react-redux-firebase";
 import { compose } from "redux";
@@ -10,14 +10,16 @@ class Dashboard extends Component {
   render() {
     const { projects, auth } = this.props;
     if (!auth.uid) return <Redirect to="/signin" />;
+
     return (
       <div className="dashboard container">
         <div className="row">
           <div className="col s12 m6">
             <ProjectList projects={projects} />
           </div>
-          <div className="col s12 m5 offset-m1">...</div>
-          <Notifications />
+          <div className="col s12 m5 offset-m1">
+            <Notifications />
+          </div>
         </div>
       </div>
     );
@@ -25,6 +27,7 @@ class Dashboard extends Component {
 }
 
 const mapStateToProps = state => {
+  // console.log(state);
   return {
     projects: state.firestore.ordered.projects,
     auth: state.firebase.auth
